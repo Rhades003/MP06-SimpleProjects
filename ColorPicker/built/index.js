aac2f443d3b56d4b794aff2baaae78590ddcb950
Rubén <rjulian@elpuig.xeill.net>
2024-10-09 20:06:15 +0200

    feature: changeColor()

---
+++ ColorPicker/built/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let btn = document.getElementById("Color");
+function changeColor() {
+    let fondo = document.getElementsByTagName("body")[0];
+    fondo.style.backgroundColor = "red";
+}
---
+++ ColorPicker/src/ts/index.ts
@@ -0,0 +1,8 @@
+let btn:HTMLElement = document.getElementById("Color")!;
+
+function changeColor(){
+    let fondo:HTMLElement = document.getElementsByTagName("body")[0]!;
+
+    fondo.style.backgroundColor = "red";
+
+}
