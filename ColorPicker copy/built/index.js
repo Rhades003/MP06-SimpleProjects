a83f7ba70945bf5f4d9148242aaee142a3904289
Rubén <rjulian@elpuig.xeill.net>
2024-10-16 20:30:20 +0200

    Practica1  david_tomas

---
+++ ColorPicker copy/built/index.js
@@ -0,0 +1,24 @@
+"use strict";
+/*declaro la variable  var_ruben especificando su tipo con ":",
+siempre especificando que he terminado con ";"para saber que he terminado la acción*/
+var var_ruben = 30;
+/* imprmimo mi super variable con el gran console.log para ver su valor,
+siempre especificando que he terminado con ";"para saber que he terminado la acción*/
+console.log(var_ruben);
+/*reasigno el valor de mi super variable sin el var por patrones de diseño y
+siempre especificando que he terminado con ";"para saber que he terminado la acción*/
+var_ruben = 10;
+/* reimprmimo mi super variable con el gran console.log para ver su valor,
+siempre especificando que he terminado con ";"para saber que he terminado la acción*/
+console.log(var_ruben);
+/*declaro la variable  let_ruben especificando su tipo con ":",
+siempre especificando que he terminado con ";"para saber que he terminado la acción*/
+let let_ruben = "\"Hola món!\"";
+/*reasigno el valor de mi super variable sin el let porque si no peta y
+siempre especificando que he terminado con ";"para saber que he terminado la acción*/
+console.log(let_ruben);
+let_ruben = "\"Adeu món!\"";
+console.log(let_ruben);
+const const_ruben = "\"Javascript obsolet\"";
+console.log(const_ruben);
+//const_ruben = "otra cosa";
