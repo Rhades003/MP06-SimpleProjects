d98c0d1e6e36b035bcbe33a4e1029d79c190129f
Rubén <rjulian@elpuig.xeill.net>
2024-12-16 19:43:52 +0100

    lista terminada

--- cla-cla-cla-clases/built/index.js
+++ cla-cla-cla-clases/built/index.js
@@ -1,6 +1,45 @@
 "use strict";
 //import { Client } from './Client';
 //import { Order } from './Order';
+const restaurant = {
+    name: "Casa Pepe",
+    ubi: "Avenida San Carlos N34",
+    plats: new Map([
+        ["Pasta", 30],
+        ["Pulpo Gallego", 40],
+        ["Hamburguesa", 23]
+    ]),
+};
+function showPepe(restaurant) {
+    if (restaurant) {
+        let name = document.createElement("h2");
+        name.innerText = restaurant.name;
+        document.body.appendChild(name);
+        let ubi = document.createElement("p");
+        ubi.innerText = restaurant.ubi;
+        document.body.appendChild(ubi);
+        let plats = document.createElement("ul");
+        for (let plat of restaurant.plats.entries()) {
+            let platLi = document.createElement("li");
+            platLi.innerText = (plat[0] + ": " + plat[1] + "€");
+            plats.appendChild(platLi);
+        }
+        document.body.appendChild(plats);
+    }
+    else {
+        let pepe = document.createElement("p");
+        pepe.innerText = "El restaurant no està obert";
+        document.body.appendChild(pepe);
+    }
+}
+function addPlat() {
+    let nameInput = document.getElementById("nameInput");
+    let name = nameInput.value;
+    let priceInput = document.getElementById("priceInput");
+    let price = priceInput.valueAsNumber;
+    restaurant.plats.set(name, price);
+    showPepe(restaurant);
+}
 class Client {
     constructor(name, surname, order) {
         this.name = name;
@@ -16,8 +55,9 @@ class Order {
 }
 Order.id = 1;
 function generate() {
-    const client = new Client("Ruben", "Julian", new Order("pasta"));
+    const client = new Client("Ruben", "Julian", new Order("Pasta"));
     const client2 = new Client("Ruben", "Julian", new Order("Hamburguesa"));
     console.log(client);
     console.log(client2);
+    showPepe(restaurant);
 }
--- cla-cla-cla-clases/src/ts/index.ts
+++ cla-cla-cla-clases/src/ts/index.ts
@@ -1,5 +1,48 @@
 //import { Client } from './Client';
 //import { Order } from './Order';
+
+const restaurant = {
+    name: "Casa Pepe",
+    ubi: "Avenida San Carlos N34",
+    plats: new Map<string, number>([
+        ["Pasta", 30],
+        ["Pulpo Gallego", 40],
+        ["Hamburguesa", 23]
+    ]),
+};
+function showPepe(restaurant?:any){
+    if(restaurant){
+        let name:HTMLHeadingElement = document.createElement("h2");
+        name.innerText = restaurant.name;
+        document.body.appendChild(name);
+
+        let ubi:HTMLParagraphElement = document.createElement("p");
+        ubi.innerText = restaurant.ubi;
+        document.body.appendChild(ubi);
+
+        let plats:HTMLUListElement = document.createElement("ul");
+        for (let plat of restaurant.plats.entries()) {
+            let platLi:HTMLLIElement = document.createElement("li");
+            platLi.innerText = (plat[0]+": "+plat[1]+"€");
+            plats.appendChild(platLi); 
+        }
+        document.body.appendChild(plats);
+    }
+    else {
+        let pepe:HTMLParagraphElement = document.createElement("p");
+        pepe.innerText = "El restaurant no està obert";
+        document.body.appendChild(pepe);
+    } 
+}
+function addPlat(){
+    let nameInput:HTMLInputElement = document.getElementById("nameInput") as HTMLInputElement;
+    let name:string = nameInput.value;
+    let priceInput:HTMLInputElement = document.getElementById("priceInput") as HTMLInputElement;
+    let price = priceInput.valueAsNumber;
+    restaurant.plats.set(name, price);
+    showPepe(restaurant);
+}
+
 class Client {
     public name:string;
     public surname:string;
@@ -23,10 +66,12 @@ class Order {
 }
 
 function generate() {
-    const client = new Client("Ruben", "Julian", new Order("pasta"));
+    const client = new Client("Ruben", "Julian", new Order("Pasta"));
     const client2 = new Client("Ruben", "Julian", new Order("Hamburguesa"));
     console.log(client);
     console.log(client2);
+
+    showPepe(restaurant);
     
 }
 
