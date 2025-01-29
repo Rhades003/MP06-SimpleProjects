30e78b6d473f7678ef66b38d9bd6cf274d26ea54
Rubén <rjulian@elpuig.xeill.net>
2025-01-29 19:59:36 +0100

    Cosas

---
+++ A.3.1.1.DAW M06 UF3 - Exercici 1 Gestió d'esdeveniments/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let barçaImg = document.getElementById("barcelona");
+let madridImg = document.getElementById("madrid");
+let barçaGol = document.getElementById("red");
+let madridGol = document.getElementById("orange");
+barçaImg.addEventListener("click", gooooooool(this.e));
+madridImg.addEventListener("click", gooooooool);
+let golesBarça = 0;
+let golesMadrid = 0;
+function gooooooool(event) {
+    if (event.id = "barçaImg")
+        ;
+}
+//# sourceMappingURL=index.js.map
---
+++ A.3.1.1.DAW M06 UF3 - Exercici 1 Gestió d'esdeveniments/index.ts
@@ -0,0 +1,15 @@
+let barçaImg:HTMLImageElement = document.getElementById("barcelona") as HTMLImageElement;
+let madridImg:HTMLImageElement = document.getElementById("madrid") as HTMLImageElement;
+
+let barçaGol:HTMLInputElement = document.getElementById("red") as HTMLInputElement;
+let madridGol:HTMLInputElement = document.getElementById("orange") as HTMLInputElement;
+
+barçaImg.addEventListener("click", gooooooool(this.e));
+madridImg.addEventListener("click", gooooooool);
+
+let golesBarça:number = 0;
+let golesMadrid:number = 0;
+function gooooooool(event):void {
+    if(event.id = "barçaImg")
+
+}
