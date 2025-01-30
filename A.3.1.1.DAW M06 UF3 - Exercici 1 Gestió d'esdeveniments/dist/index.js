0bf250df77be141512ebcbe702265b52a0bfd885
Rubén <rjulian@elpuig.xeill.net>
2025-01-30 18:59:44 +0100

    listener y esas cosas

--- A.3.1.1.DAW M06 UF3 - Exercici 1 Gestió d'esdeveniments/dist/index.js
+++ A.3.1.1.DAW M06 UF3 - Exercici 1 Gestió d'esdeveniments/dist/index.js
@@ -1,14 +1,82 @@
 "use strict";
 let barçaImg = document.getElementById("barcelona");
 let madridImg = document.getElementById("madrid");
-let barçaGol = document.getElementById("red");
-let madridGol = document.getElementById("orange");
-barçaImg.addEventListener("click", gooooooool(this.e));
-madridImg.addEventListener("click", gooooooool);
+let marcadorBarça = document.getElementById("red");
+let marcadorMadrid = document.getElementById("orange");
 let golesBarça = 0;
 let golesMadrid = 0;
-function gooooooool(event) {
-    if (event.id = "barçaImg")
-        ;
-}
+let marcadorFaltasBarça = document.getElementById("brown");
+let marcadorFaltasMadrid = document.getElementById("purple");
+let faltasBarça = 0;
+let faltasMadrid = 0;
+let goleadoresContador = document.getElementById("green");
+let minutosContador = document.getElementById("blue");
+let goleadores = "";
+let minutos = 0;
+//SUMAR T RESTAR GOOOOOOOOOLES
+barçaImg.addEventListener("mousedown", (e) => {
+    switch (e.button) {
+        case 0:
+            golesBarça++;
+            marcadorBarça.innerText = golesBarça.toString();
+            break;
+        case 2:
+            if (golesBarça != 0)
+                golesBarça--;
+            marcadorBarça.innerText = golesBarça.toString();
+            break;
+    }
+});
+madridImg.addEventListener("mouseup", (e) => {
+    switch (e.button) {
+        case 0:
+            golesMadrid++;
+            marcadorMadrid.innerText = golesMadrid.toString();
+            break;
+        case 2:
+            if (golesMadrid != 0)
+                golesMadrid--;
+            marcadorMadrid.innerText = golesMadrid.toString();
+            break;
+    }
+});
+barçaImg.addEventListener("mouseenter", function () {
+    faltasBarça++;
+    marcadorFaltasBarça.innerText = faltasBarça.toString();
+});
+madridImg.addEventListener("mouseleave", function () {
+    faltasMadrid++;
+    marcadorFaltasMadrid.innerText = faltasMadrid.toString();
+});
+document.body.addEventListener("keypress", function (e) {
+    goleadores += e.key.toString();
+    goleadores.concat("concat");
+    if (e.key != "0") {
+        goleadoresContador.innerText = goleadores;
+    }
+    else {
+        goleadoresContador.innerHTML = "";
+        goleadores = "";
+    }
+});
+document.body.addEventListener("dblclick", function () {
+    minutos++;
+    minutosContador.innerText = minutos.toString();
+});
+// madridImg.addEventListener("click", function(){
+//     golesMadrid++
+//     marcadorMadrid.innerText = golesMadrid.toString();
+// }); 
+// //RESTAR GOOOOOOOLES
+// barçaImg.addEventListener("auxclick", function(){
+//     golesBarça--
+//     marcadorBarça.innerText = golesBarça.toString();
+//  }); 
+//  madridImg.addEventListener("auxclick", function(){
+//      golesMadrid--
+//      marcadorMadrid.innerText = golesMadrid.toString();
+//  }); 
+//madridImg.addEventListener("click", gooooooool);
+//function gooooooool():void {
+//}
 //# sourceMappingURL=index.js.map
--- A.3.1.1.DAW M06 UF3 - Exercici 1 Gestió d'esdeveniments/index.ts
+++ A.3.1.1.DAW M06 UF3 - Exercici 1 Gestió d'esdeveniments/index.ts
@@ -1,15 +1,101 @@
 let barçaImg:HTMLImageElement = document.getElementById("barcelona") as HTMLImageElement;
 let madridImg:HTMLImageElement = document.getElementById("madrid") as HTMLImageElement;
 
-let barçaGol:HTMLInputElement = document.getElementById("red") as HTMLInputElement;
-let madridGol:HTMLInputElement = document.getElementById("orange") as HTMLInputElement;
-
-barçaImg.addEventListener("click", gooooooool(this.e));
-madridImg.addEventListener("click", gooooooool);
+let marcadorBarça:HTMLInputElement = document.getElementById("red") as HTMLInputElement;
+let marcadorMadrid:HTMLInputElement = document.getElementById("orange") as HTMLInputElement;
 
 let golesBarça:number = 0;
 let golesMadrid:number = 0;
-function gooooooool(event):void {
-    if(event.id = "barçaImg")
 
+let marcadorFaltasBarça:HTMLInputElement = document.getElementById("brown") as HTMLInputElement;
+let marcadorFaltasMadrid:HTMLInputElement = document.getElementById("purple") as HTMLInputElement;
+
+let faltasBarça:number = 0;
+let faltasMadrid:number = 0;
+
+let goleadoresContador:HTMLInputElement = document.getElementById("green") as HTMLInputElement;
+let minutosContador:HTMLInputElement = document.getElementById("blue") as HTMLInputElement;
+
+let goleadores:string = "";
+let minutos:number = 0;
+
+
+//SUMAR T RESTAR GOOOOOOOOOLES
+
+barçaImg.addEventListener("mousedown", (e) => {
+    switch (e.button) {
+        case 0: 
+            golesBarça++;
+            marcadorBarça.innerText = golesBarça.toString();
+            break;
+
+        case 2: 
+            if(golesBarça != 0) golesBarça--;
+            marcadorBarça.innerText = golesBarça.toString();
+            break;
+    }
+}); 
+
+madridImg.addEventListener("mouseup", (e) => {
+    switch (e.button) {
+        case 0: 
+            golesMadrid++;
+            marcadorMadrid.innerText = golesMadrid.toString();
+            break;
+
+        case 2: 
+            if(golesMadrid != 0) golesMadrid--;
+            marcadorMadrid.innerText = golesMadrid.toString();
+            break;
+    }
+});
+
+barçaImg.addEventListener("mouseenter", function() {
+    faltasBarça++;
+    marcadorFaltasBarça.innerText = faltasBarça.toString();
+});
+
+madridImg.addEventListener("mouseleave", function() {
+    faltasMadrid++;
+    marcadorFaltasMadrid.innerText = faltasMadrid.toString();
+});
+
+document.body.addEventListener("keypress", function(e) {
+    goleadores+=e.key.toString()
+    goleadores.concat("concat")
+
+    if(e.key != "0") {
+        goleadoresContador.innerText = goleadores;
+    }
+    else {
+        goleadoresContador.innerHTML = "";
+        goleadores = "";
 }
+});
+
+document.body.addEventListener("dblclick", function() {
+    minutos++;
+    minutosContador.innerText = minutos.toString();
+});
+// madridImg.addEventListener("click", function(){
+//     golesMadrid++
+//     marcadorMadrid.innerText = golesMadrid.toString();
+// }); 
+
+// //RESTAR GOOOOOOOLES
+// barçaImg.addEventListener("auxclick", function(){
+//     golesBarça--
+//     marcadorBarça.innerText = golesBarça.toString();
+//  }); 
+ 
+//  madridImg.addEventListener("auxclick", function(){
+//      golesMadrid--
+//      marcadorMadrid.innerText = golesMadrid.toString();
+//  }); 
+
+//madridImg.addEventListener("click", gooooooool);
+
+
+//function gooooooool():void {
+
+//}
